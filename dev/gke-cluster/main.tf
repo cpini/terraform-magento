b44ce0d60044f7a9633de4727a8732747f13c873
Cris Pini <cris_pini@yahoo.co.uk>
2020-12-18 16:36:21 +0000

    Added additional resources

--- dev/gke-cluster/main.tf
+++ dev/gke-cluster/main.tf
@@ -34,14 +34,14 @@ module "gke_cluster" {
   # to a specific version of the modules, such as the following example:
   # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.2.0"
   #source = "../../modules/gke-cluster"
-  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.4.3"	
+  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-cluster?ref=v0.6.0"	
 
   name = var.cluster_name
 
   project  = var.project
   location = var.location
   network  = module.vpc_network.network
-
+  
   # We're deploying the cluster in the 'public' subnetwork to allow outbound internet access
   # See the network access tier table for full details:
   # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
@@ -72,7 +72,7 @@ module "gke_cluster" {
   cluster_secondary_range_name = module.vpc_network.public_subnetwork_secondary_range_name
 
   enable_vertical_pod_autoscaling = var.enable_vertical_pod_autoscaling
-
+  
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -83,7 +83,7 @@ module "gke_service_account" {
   # to a specific version of the modules, such as the following example:
   # source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.2.0"
   #source = "../../modules/gke-service-account"
-  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.4.3"
+  source = "github.com/gruntwork-io/terraform-google-gke.git//modules/gke-service-account?ref=v0.6.0"
 
   name                    = var.cluster_service_account_name
   project                 = var.project
@@ -169,7 +169,7 @@ resource "google_container_node_pool" "node_pool" {
       "private-pool-magento",
     ]
 
-    disk_size_gb = "30"
+    disk_size_gb = "10"
     disk_type    = "pd-standard"
     preemptible  = false
 
--- dev/gke-cluster/variables.tf
+++ dev/gke-cluster/variables.tf
@@ -6,7 +6,7 @@
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
-  default     = "magento2-269418"
+  default     = "magento-295411"
 }
 
 variable "location" {
